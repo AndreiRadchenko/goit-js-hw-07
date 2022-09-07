89657da152205d86fd98b892b11aa0b6fe646682
AndreiRadchenko <d4100kx@gmail.com>
2022-09-08 00:44:08 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+let basicLightboxInstance;
+
+function createGalleryMarkup(galleryItemsArray) {
+  return galleryItemsArray
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href=${original} 
+                    target="_blank" rel="noreferrer noopener">
+                  <img
+                    class="gallery__image"
+                    src=${preview}
+                    data-source=${original}
+                    alt=${description}
+                  />
+                </a>
+              </div>`;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  basicLightboxInstance = createBasicLightbox(event.target.dataset.source);
+  basicLightboxInstance.show();
+}
+
+function createBasicLightbox(imgSource) {
+  return basicLightbox.create(
+    `
+    <img src="${imgSource}" width="800" height="600">
+`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onEscapeKeyDown);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscapeKeyDown);
+      },
+    },
+  );
+}
+
+function onEscapeKeyDown(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  basicLightboxInstance.close();
+}
